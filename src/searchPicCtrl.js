ca65da75f5f9d4f8d99ed8c8d7f6c7b394cff072
ManishaKundoor-Reflektion <manisha.kundoor@reflektion.com>
2016-10-03 10:40:47 -0700

    aspect ratio

--- src/searchPicCtrl.js
+++ src/searchPicCtrl.js
@@ -1,6 +1,6 @@
 angular.module('searchPicture', ['ngFileUpload'])
-.controller('searchPicCtrl',['$scope', '$q', 'Upload',
-    function($scope, $q, Upload) {
+.controller('searchPicCtrl',['$scope', '$q', '$window', 'Upload',
+    function($scope, $q, $window, Upload) {
         $scope.name = 'Take Picture';
         $scope.products = [];
         $scope.tags = [];
@@ -20,33 +20,55 @@ angular.module('searchPicture', ['ngFileUpload'])
                 type: 'image/jpeg',
                 centerCrop: false
             };
-            $scope.file = files[0];
-            $scope.save(files[0], options).then(function (res) {
-                $scope.showHeader1 = false;
 
-                if (typeof(res.products) === 'object') {
-                    $scope.productsData = res.products;
-                    $scope.products = Object.keys(res.products).map(function(key) {
-                        return res.products[key];
-                    });
-                } else {
-                    $scope.products = res;
-                }
+            var _URL = $window.URL || $window.webkitURL;
+            var img = new Image();
+            img.src = _URL.createObjectURL(files[0]);
+            img.onload = function () {
+                console.log(img.width + " " + img.height);
 
-                if (res.tags && Object.keys(res.tags).length > 0) {
-                    $scope.tagsData = res.tags;
-                    var tags = ['all'];
-                    Object.keys(res.tags).forEach(function(tag) {
-                        if (tag != 'all') {
-                            tags.push(tag);
-                        }
-                    });
-                    $scope.tags = tags;
-                    $scope.products = (res.tags.all).map(function(key) {
-                        return $scope.productsData[key];
-                    });
-                }
-            });
+                 if (img.width > 512 || img.height > 512) {
+                     if (img.width < img.height) {
+                         options.width = 512;
+                         options.height = options.width * img.height / img.width;
+                     } else {
+                         options.height = 512;
+                         options.width = options.height * img.width / img.height;
+                     }
+                 } else {
+                     options.width = img.width;
+                     options.height = img.height;
+                 }
+
+                console.log(options.width + " " + options.height);
+
+                $scope.save(files[0], options).then(function (res) {
+                    $scope.showHeader1 = false;
+
+                    if (typeof(res.products) === 'object') {
+                        $scope.productsData = res.products;
+                        $scope.products = Object.keys(res.products).map(function(key) {
+                            return res.products[key];
+                        });
+                    } else {
+                        $scope.products = res;
+                    }
+
+                    if (res.tags && Object.keys(res.tags).length > 0) {
+                        $scope.tagsData = res.tags;
+                        var tags = ['all'];
+                        Object.keys(res.tags).forEach(function(tag) {
+                            if (tag != 'all') {
+                                tags.push(tag);
+                            }
+                        });
+                        $scope.tags = tags;
+                        $scope.products = (res.tags.all).map(function(key) {
+                            return $scope.productsData[key];
+                        });
+                    }
+                });
+            };
         };
 
         $scope.sortByTag = function (tag) {
@@ -59,6 +81,7 @@ angular.module('searchPicture', ['ngFileUpload'])
             var deferred = $q.defer();
             //options.resizeIf;//to-do do not resize if already less than 512
             Upload.resize(file, options.width, options.height, options.quality, options.type, null, options.centerCrop).then(function (resizedFile) {
+                $scope.file = resizedFile;
                 Upload.upload({
                     url: '/api/searchPicture/save',
                     method: 'POST',
